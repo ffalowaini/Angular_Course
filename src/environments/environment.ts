5125cb808b5327a3160e886d853f3c9794b7213b
Faisal <ffalowaini@gmail.com>
2021-09-07 11:22:01 +0300

    adding environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyC9dRX6DOMnPC4gChYYwHEQIWKlF7Qw_g8',
+
 };
 
 /*
